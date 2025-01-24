810efa2f0bee584f628caa4650b4ab43ae854968
carlos22709 <carlossanchez22709@gmail.com>
2025-01-24 14:37:44 -0500

    :sparkles: modelos

---
+++ models.py
@@ -0,0 +1,56 @@
+import json
+from dataclasses import dataclass, field
+from typing import List
+
+@dataclass
+class Miembro:
+    id: int
+    nombre: str
+    apellidos: str
+    num_identificacion: str
+    correo: str
+    telefono: str
+    estado: str
+
+@dataclass
+class Informe:
+    id: int
+    miembro_id: int
+    clases_mes: int
+    clases_asistidas: int
+    torneos_asistidos: int
+
+@dataclass
+class Entrenamiento:
+    id: int
+    fecha: str  # Formato 'AAAA-MM-DD'
+
+@dataclass
+class Asistencia_Entrenamiento:
+    id: int
+    miembro_id: int
+    entrenamiento_id: int
+    estado: str  # 'verde' o 'rojo'
+
+@dataclass
+class Torneo:
+    id: int
+    nombre: str
+
+@dataclass
+class Asistencia_Torneo:
+    id: int
+    miembro_id: int
+    torneo_id: int
+
+@dataclass
+class Inscripcion:
+    id: int
+    miembro_id: int
+    pago_id: int
+
+@dataclass
+class Pago:
+    id: int
+    estado: str
+    concepto: str
