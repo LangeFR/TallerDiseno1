ed313d8f91b95731624b880bffc48e781ad46626
carlos22709 <carlossanchez22709@gmail.com>
2025-01-25 12:04:57 -0500

    método innecesario

--- modelos/matriculas.py
+++ modelos/matriculas.py
@@ -27,15 +27,3 @@ class Matricula:
         with open("base_de_datos/matriculas.json", "w") as archivo:
             json.dump(matriculas, archivo, indent=4)
 
-    def registrar_matriculas(self):
-        try:
-            with open("base_de_datos/usuarios.json", "r") as archivo:
-                usuarios = json.load(archivo)
-        except (FileNotFoundError, json.JSONDecodeError):
-            usuarios = []
-
-        for usuario in usuarios:
-            # asignamos un nuevo pago_id
-            nuevo_pago_id = self.nuevo_id()  # Esto puede cambiarse según cómo se manejen los pagos
-            matricula = Matricula(id=self.nuevo_id(), usuario_id=usuario['id'], pago_id=nuevo_pago_id)
-            matricula.guardar()
