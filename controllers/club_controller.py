03352900b0ce11c3c92191ddb5c68a464f2036cb
carlos22709 <carlossanchez22709@gmail.com>
2025-01-30 21:03:40 -0500

    :bug: Arreglado dropdown de pagos

--- controllers/club_controller.py
+++ controllers/club_controller.py
@@ -49,7 +49,7 @@ class ClubController:
         """
         Retorna un Dropdown con los usuarios que están matriculados en el club.
         """
-        usuarios_matriculados = [u for u in self.usuarios if u.estado == "matriculado"]
+        usuarios_matriculados = [u for u in self.usuarios]
         return ft.Dropdown(
             label="Seleccionar Usuario",
             options=[ft.dropdown.Option(u.nombre) for u in usuarios_matriculados],
